90e830ce604fd20b262abc535f3df9278489fc31
irshad-sme <irshad@searchmyexpert.com>
2024-05-08 18:27:09 +0530

    take pull from main

--- app/users/[user]/page.js
+++ app/users/[user]/page.js
@@ -1,24 +1,31 @@
 import { redirect } from "next/navigation";
 import React from "react";
-async function fetchData(id) {
-  let responce = await fetch(`http://localhost:3000/api/dbuser/${id}`);
-  responce = await responce.json();
-  return responce[0];
+
+ async function fetchData(id) {
+  const response = await fetch(`http://localhost:3000/api/dbuser/${id}`);
+  const res = await response.json();
+  return res.result;
 }
+
 export default async function page({ params }) {
-  const singleUser = params.user;
+
+  console.log("----------->",params)
+  const singleUser = params?.user;
+
   const userData = await fetchData(singleUser);
-  if(userData.page=="404"){
+
+  if(userData?.page===404){
     redirect("/not-found")
   }
+
   return (
     <div>
        {
                 <div className='text-center'>
                     <div className='p-3 flex flex-col items-center gap-3'>
-                    <img src={userData.avatar} alt={userData.first_name} className='w-28 h-28 object-cover rounded-full overflow-hidden'/>
-                    <p className='font-bold'>{userData.first_name} {userData.last_name}</p>
-                    <p>{userData.email}</p>
+                    <img src={userData?.avatar} alt={userData?.first_name} className='w-28 h-28 object-cover rounded-full overflow-hidden'/>
+                    <p className='font-bold'>{userData?.first_name} {userData?.last_name}</p>
+                    <p>{userData?.email}</p>
                     </div>
                 </div>
         }
