fad9ee3bf38f35ad2ea00f3f359bb13cc8b241a8
irshad-sme <irshad@searchmyexpert.com>
2024-06-05 18:50:51 +0530

    add design in product name in redux

--- app/redux/use-redux/testing/page.js
+++ app/redux/use-redux/testing/page.js
@@ -9,7 +9,12 @@ export default function Page() {
       <h2 className="text-xl font-semibold">All Products Record</h2>
       <ul className="grid grid-cols-1 md:grid-cols-4 gap-3 mt-5 text-gray-600">
         {productData.map((item) => (
-          <li key={item.id}>{item.name}</li>
+          <li
+            key={item.id}
+            className="bg-slate-300 text-black px-3 py-1 rounded-full"
+          >
+            {item.name}
+          </li>
         ))}
       </ul>
     </div>
